d27f839038bff866e3b360977911f28985e70428
Arpit Saxena <arpit.tarang@gmail.com>
2025-08-30 12:39:30 +0530

    add brevo + upgrade heroku image

--- server/api/email.js
+++ server/api/email.js
@@ -1,7 +1,9 @@
 const sgMail = require('@sendgrid/mail')
+const brevo = require('@getbrevo/brevo');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-var sendEmail = (pdfString, toEmail, fromEmail, text) => {
+// old api, now sendgrid is asking for money so switched to brevo
+var sendEmailSendgrid = (pdfString, toEmail, fromEmail, text) => {
   const msg = {
     to: toEmail, // Change to your recipient
     from: fromEmail, // Change to your verified sender
@@ -21,6 +23,34 @@ var sendEmail = (pdfString, toEmail, fromEmail, text) => {
     .then(() => {
       console.log('Email sent')
     })
+}
+
+
+
+let apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.authentications.apiKey.apiKey = process.env.BREVO_API_KEY;
+
+var sendEmail = (pdfString, toEmail, fromEmail, text) => {
+  let sendSmtpEmail = new brevo.SendSmtpEmail();
+  
+  sendSmtpEmail.subject = `Invoice - Dr. Anurag Saxena's Clinic`;
+  sendSmtpEmail.htmlContent = text;
+  sendSmtpEmail.sender = { "name": "Dr. Anurag Saxena", "email": fromEmail };
+  sendSmtpEmail.to = [
+    { "email": toEmail }
+  ];
+  sendSmtpEmail.attachment = [
+    {
+      content: pdfString,
+      name: "invoice.pdf",
+    }
+  ]
+  
+  apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+  }, function (error) {
+    console.error(error);
+  });
 }
 
 module.exports = {
